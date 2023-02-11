ce5c12776988f26130dd2d44d70d2afcd8979d7e
Lincon Rezende <lincon.gomes.cfpt@gmail.com>
2023-02-11 09:43:27 -0300

    Added a default response and error to BaseController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,27 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+  use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+  public function sendResponse($result, $message)
+  {
+    $response = [
+      'success' => true,
+      'data'  => $result,
+      'message' => $message,
+    ];
+    return response()->json($response, 200);
+  }
+  
+  public function sendError($error, $errorMessages = [], $code = 404)
+  {
+    $response = [
+      'success' => false,
+      'message' => $error,
+    ];
+    if (!empty($errorMessages)) {
+      $response['data'] = $errorMessages;
+    }
+    return response()->json($response, $code);
+  }
 }
