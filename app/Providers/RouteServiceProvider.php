3313f89b0386dcc43f0f86c00fd26e474765e8e6
Lincon Rezende <lincon.gomes.cfpt@gmail.com>
2023-02-11 09:42:44 -0300

    Added v1 route on RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,43 +10,64 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * The path to the "home" route for your application.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
+  /**
+   * The path to the "home" route for your application.
+   *
+   * Typically, users are redirected here after authentication.
+   *
+   * @var string
+   */
+  public const HOME = '/home';
 
-    /**
-     * Define your route model bindings, pattern filters, and other route configuration.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->configureRateLimiting();
+  /** @var string $apiNamespace */
+  protected $apiNamespace = 'App\Http\Controllers';
+  
+  /**
+   * Define your route model bindings, pattern filters, and other route configuration.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->configureRateLimiting();
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+    // $this->routes(function () {
+    //   Route::middleware('api')
+    //     ->prefix('api')
+    //     ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
-    }
+    //   Route::middleware('web')
+    //     ->group(base_path('routes/web.php'));
+    // });
+    $this->mapApiRoutes();
+  }
 
-    /**
-     * Configure the rate limiters for the application.
-     *
-     * @return void
-     */
-    protected function configureRateLimiting()
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
-    }
+  /**
+   * Configure the rate limiters for the application.
+   *
+   * @return void
+   */
+  protected function configureRateLimiting()
+  {
+    RateLimiter::for('api', function (Request $request) {
+      return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+    });
+  }
+
+  /**
+   * Define the "api" routes for the application.
+   *
+   * These routes are typically stateless.
+   *
+   * @return void
+   */
+  protected function mapApiRoutes()
+  {
+    Route::group([
+      'namespace'  => $this->apiNamespace.'\v1',
+      'prefix'   => 'api/v1',
+    ], function ($router) {
+      require base_path('routes/api_v1.php');
+    });
+  }
 }
